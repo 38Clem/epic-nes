b0b756c27416d4dc07ec139810d6736d0fbeb6ff
Clemence <38Clem@users.noreply.github.com>
2020-04-07 18:44:32 +0200

    Try to play some music on Start Page, still not working ... WIP

--- src/Component/AudioPlayer.jsx
+++ src/Component/AudioPlayer.jsx
@@ -3,16 +3,14 @@ import { Component } from "react";
 
 export default class AudioPlayer extends Component {
 
-  music = new Audio("./../assets/sounds/adventure-begins.ogg")
+  audio = new Audio("./assets/sounds/adventure-begins.ogg")
  
-  
-
   render = () => {
 
     return (
       <audio
       controls
-      onPlay={() => this.music.play}
+      onPlay={() => this.audio.play()}
       onError={e => console.log(e)}
     />
 
